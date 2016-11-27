46e7f8f27599f2fd786f4f12c79900b767a3b996
Dwaipayan <dwaipayan2609@gmail.com>
2016-11-27 13:49:27 +0530

    added similarity retrieval and modified both servers

--- recommender/Retrieve_values.py
+++ recommender/Retrieve_values.py
@@ -9,8 +9,9 @@ client=MongoClient()
 db=client.temp
 content=db.content
 id = 0
-for i in content.find({"category":"national"}):
+for i in content.find({"category":"health"}):
 	f = open('/home/mera_naam_dwaipayan/Dwaipayan/Zeus/src/client/json/' + 'national.json', 'a')
+	print(i)
 	i['id'] = id
 	id = id + 1
 	i['upvotes'] = 0
